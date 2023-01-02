18ce38ae0ac916931e0189d5b84b88679127a061
Jacson Curtis <justjcurtis@gmail.com>
2023-01-02 00:38:22 +0000

    feat: timeleaf title coloring

--- src/components/timeleaf.js
+++ src/components/timeleaf.js
@@ -5,7 +5,7 @@ const TimeLeaf = ({ date, title, text, isRight = false }) => {
 
             <div className={`order-1  w-5/12 px-1 py-4 ${isRight ? 'text-left' : 'text-right'}`}>
                 <p className={`mb-3 text-base text-accent ${isRight ? 'text-left' : 'text-right'}`}>{date}</p>
-                <h4 className={`mb-3 font-bold  text-lg md:text-2xl ${isRight ? 'text-left' : 'text-right'}`}>{title}</h4>
+                <h4 className={`mb-3 font-bold  text-lg md:text-2xl text-primary bg-primary-content rounded-md px-1 ${isRight ? 'text-left' : 'text-right'}`}>{title}</h4>
                 <p className={`text-sm md:text-base leading-snug text-gray-50 text-opacity-100 ${isRight ? 'text-left' : 'text-right'}`}>
                     {text}
                 </p>
