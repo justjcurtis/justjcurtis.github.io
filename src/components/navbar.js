7e8c65c0c527e6556fd19867d8826ac8955b2e83
Jacson Curtis <justjcurtis@gmail.com>
2023-01-02 20:02:48 +0000

    feat: refactor nav + styling update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 const iconImagePath = require('../assets/favicon.png')
 
 const styles = {
@@ -8,19 +8,29 @@ const styles = {
     }
 }
 
-const Navbar = () => {
 
+const Navbar = () => {
+    const location = useLocation().pathname
+    const links = [
+        { title: 'Home', to: '/' },
+        { title: 'Projects', to: '/projects' },
+        { title: 'About', to: '/about' },
+    ]
+    const ListItemLink = (title, to, location, mx = 0) => <li key={title} className={`rounded-md mx-${mx} ${location === to ? 'bg-secondary text-neutral-focus' : ''}`}><Link className="text-center inline" to={to}>{title}</Link></li>
     return (
-        <div className="navbar bg-base-100">
+        <div onClick={() => console.log('clicked')} className="navbar fixed z-[100] bg-base-100">
             <div className="navbar-start">
-                <div className="dropdown">
+                <div className="invisible w-0 md:w-full md:visible">
+                    <ul className="menu menu-horizontal px-1">
+                        {links.map(({ title, to }) => ListItemLink(title, to, location, 1))}
+                    </ul>
+                </div>
+                <div className="visible w-full dropdown md:w-0 md:invisible ">
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                        <li><Link to="/about">About</Link></li>
+                        {links.map(({ title, to }) => ListItemLink(title, to, location))}
                     </ul>
                 </div>
             </div>
