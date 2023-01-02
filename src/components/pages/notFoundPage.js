21871c7e76ddbea302f4b7ebed6b4b03ac050bc6
Jacson Curtis <justjcurtis@gmail.com>
2023-01-02 01:23:10 +0000

    fix: add br to last line of not found text

--- src/components/pages/notFoundPage.js
+++ src/components/pages/notFoundPage.js
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
             <div className="hero-content text-center">
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">Oops, my 404 is showing 😳</h1>
-                    <p className="py-6"><span className="line-through">This page doesn't exist 💨,</span> well.. <span className="italic">this</span> page exists but, it just not the page you were looking 🔭 for. <br /><br />..<span className="italic">Unless</span>, you <span className="font-bold underline">knew</span> the page that you were going to didn't exist & your intentions were actually to 👀 see my 404 🤔. In that case.. well done ? I think 🤷🏻‍♂️</p>
+                    <p className="py-6"><span className="line-through">This page doesn't exist 💨,</span> well.. <span className="italic">this</span> page exists but, it just not the page you were looking 🔭 for. <br /><br />..<span className="italic">Unless</span>, you <span className="font-bold underline">knew</span> the page that you were going to didn't exist & your intentions were actually to 👀 see my 404 🤔. In that case.. well done ? <br /> I think 🤷🏻‍♂️</p>
                 </div>
             </div>
         </div>
