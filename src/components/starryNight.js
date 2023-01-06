3885f0eb25b640c58f948afca24be539dff68e8a
Jacson Curtis <justjcurtis@gmail.com>
2023-01-06 17:54:54 +0000

    perf: remove react-p5 replace star background for about page

--- src/components/starryNight.js
+++ src/components/starryNight.js
@@ -1,13 +1,62 @@
-import { useRef } from "react";
-import Sketch from "react-p5";
-import { setup, draw } from './starfield/sketch'
+import { useCallback, useEffect, useRef, useState } from "react"
+import { getRandomInt } from "../utils/helpers"
+
+const TWINKLE_RATE = 0.0008
 
 const StarryNight = ({ children }) => {
-    const canvasParentRef = useRef()
+    const randomXY = () => {
+        return { x: getRandomInt(window.innerWidth), y: getRandomInt(window.innerHeight * 2) }
+    }
+    const starCount = useRef(Math.floor((window.innerWidth * window.innerHeight) / 4000))
+    const [stars, setStars] = useState(new Array(starCount.current).fill(0).map(() => ({ ...randomXY(), size: getRandomInt(2, 0.5), opacity: 1 })))
+    const twinkling = useRef({})
+
+    const draw = useCallback(() => {
+        setStars((currentStars) => {
+            if (!currentStars) return
+            const nextStars = []
+            for (let i = 0; i < starCount.current; i++) {
+                const s = currentStars[i]
+                if (twinkling.current[i] !== undefined) {
+                    twinkling.current[i]--
+                    if (twinkling.current[i] === 0) {
+                        twinkling.current[i] = undefined
+                        s.opacity = 1
+                    }
+                } else {
+                    if (Math.random() >= TWINKLE_RATE) {
+                        nextStars.push(s)
+                        continue
+                    }
+                    twinkling.current[i] = getRandomInt(20, 40)
+                    s.opacity = 0
+                }
+                nextStars.push(s)
+            }
+            return nextStars
+        })
+    }, [starCount])
+
+    useEffect(() => {
+        setTimeout(draw, 10)
+    }, [stars, draw])
     return (
         <>
-            <Sketch setup={setup} draw={draw} className="fixed top-0 mx-auto left-0 right-0" />
-            <div ref={canvasParentRef} className="bg-transparent text-white py-8 z-50 absolute top-0 mx-auto left-0 right-0">
+            <div className="bg-transparent text-white py-8 z-50 sticky top-0 mx-auto left-0 right-0 overflow-hidden">
+                {stars && stars.map((s, i) => {
+                    return (
+                        <div key={i} id={`star-${i}`} style={
+                            {
+                                opacity: s.opacity,
+                                top: `${s.y - 200 + (-window.scrollY * 0.1)}px`,
+                                left: `${s.x}px`,
+                                backgroundXolor: `white`,
+                                width: `${s.size}px`,
+                                height: `${s.size}px`,
+                            }
+                        } className={`starticle fixed bg-white transition-opacity duration-300`} />
+                    )
+                })}
                 {children}
             </div>
         </>
