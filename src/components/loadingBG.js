3e3302f141d2dfeae34d3bbf0ba34da58587bc05
Jacson Curtis <justjcurtis@gmail.com>
2023-01-06 13:13:41 +0000

    feat: animated loading screen bg

---
+++ src/components/loadingBG.js
@@ -0,0 +1,13 @@
+export const LoadingBG = () => {
+    return (
+        <>
+            <div className="page-bg absolute top-0 left-0 right-0 bottom-0"></div>
+            <div className="animation-wrapper">
+                <div className="particle particle-1"></div>
+                <div className="particle particle-2"></div>
+                <div className="particle particle-3"></div>
+                <div className="particle particle-4"></div>
+            </div>
+        </>
+    )
+}
