9f4ef381671b9083950873410c3973604625e633
justjcurtis <justjcurtis@gmail.com>
2023-07-12 18:26:28 +0100

    feat: add current focus; fix: homepage text formatting

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -27,18 +27,18 @@ const HomePage = () => {
                 <div className="max-w-md">
                     <m.h1 initial={{ opacity: 0, x: -2, y: -10 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.4, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: 2, y: -10 }} className="mb-5 text-5xl font-bold"><span className='text-primary bg-primary-content rounded-md px-1'>Hi 👋🏼</span></m.h1>
                     <div style={styles.topContainer}>
-                        <m.p initial={{ opacity: 0, x: -10, y: 2 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.25, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: -10, y: 2 }} style={styles.subtext} className="text-left mb-5">Welcome to my personal site. Here you can find <Link to={'/projects'}><span className='text-accent bg-primary-content rounded px-1'>things I've made</span></Link>. There are links to my other places on the internet in the top right. You can see some info <Link to={'/about'}><span className='text-accent bg-primary-content rounded px-1'>about me</span></Link> too.</m.p>
+                        <m.p initial={{ opacity: 0, x: -10, y: 2 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.25, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: -10, y: 2 }} style={styles.subtext} className="text-left mb-5">Welcome to my personal site. Here you can find <Link to={'/projects'}><span className='text-secondary underline bg-neutral rounded px-1'>things I've made</span></Link>. There are links to my other places on the internet in the top right. You can see some info <Link to={'/about'}><span className='text-secondary bg-neutral underline rounded px-1'>about me</span></Link> too.</m.p>
                         <m.img initial={{ opacity: 0, x: +10, y: 6 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.2, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: 6, y: 10 }} style={styles.avatarImage} className="mask mask-circle border-4 border-neutral-content rounded-full" src={images.Avatar} alt="just j curtis" />
                     </div>
-                    <m.div initial={{ opacity: 0, x: -3, y: 15 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.3, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: -3, y: 15 }} className="stats shadow bg-opacity-60 backdrop-blur-sm">
-                        {/* <div className="stat">
-                            <div className="stat-figure text-secondary">
+                    <m.div initial={{ opacity: 0, x: -6, y: 25 }} animate={{ opacity: 1, x: 0, y: 10 }} transition={{ delay: 0.3, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: -3, y: 15 }} className="stats shadow bg-opacity-60 backdrop-blur-sm">
+                        <div className="stat">
+                            {/* <div className="stat-figure text-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                            </div>
-                            <div className="stat-title">Views This Month</div>
-                            <div className="stat-value text-secondary">{views}</div>
-                            <div className="stat-desc">Thanks for stopping by   🌈</div>
-                        </div> */}
+                            </div> */}
+                            <div className="stat-title">Current Focus</div>
+                            <div className="stat-value text-accent underline"><a href='https://www.zopa.com/help/article/how-to-download-zopa-app' target="_blank" rel="noreferrer">Zopa App</a></div>
+                            <div className="stat-desc mt-3">Thanks for stopping by   🌈</div>
+                        </div>
 
                     </m.div>
                 </div>
