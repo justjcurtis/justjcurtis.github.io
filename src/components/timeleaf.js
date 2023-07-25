e43058875532f3b4507b479fc58707a580d0530a
justjcurtis <justjcurtis@gmail.com>
2023-07-25 07:59:42 +0100

    fix: timeleaf marker zindex

--- src/components/timeleaf.js
+++ src/components/timeleaf.js
@@ -9,9 +9,9 @@ const TimeLeaf = ({ date, title, text, isRight = false }) => {
     const trandom = useRef(Math.random())
     return (
         <m.div initial={{ opacity: 0, x: xPre }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 0.5 + (0.2 * Math.random()), duration: (0.1 + (0.3 * trandom.current)), ease: 'easeInOut' }} exit={{ opacity: 0, x: xPre }} className={`mb-8 flex justify-between items-center w-full ${isRight ? 'left-timeline' : 'flex-row-reverse right-timeline'}`}>
-            <div style={markerStyle} className={`w-full h-1 ${gradientClassName} from-accent via-transparent to-transparent`} />
+            <div style={markerStyle} className={`w-full h-1 ${gradientClassName} from-accent via-transparent to-transparent z-0`} />
             <div className="order-1 w-5/12"></div>
-            <div className={`order-1  w-5/12 px-1 py-4 ${textSideClassName}`}>
+            <div className={`order-1  w-5/12 px-1 py-4 z-20 ${textSideClassName}`}>
                 <p className={`mb-3 text-base text-accent ${textSideClassName}`}>{date}</p>
                 <h4 className={`mb-3 font-bold  text-lg md:text-2xl text-primary bg-primary-content rounded-md px-1 ${textSideClassName}`}>{title}</h4>
                 <p className={`text-sm md:text-base leading-snug text-gray-50 text-opacity-100 ${textSideClassName}`}>
