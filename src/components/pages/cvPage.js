a1da0ad83f334de5c79011c9682de15da2fb6caa
justjcurtis <justjcurtis@gmail.com>
2023-08-07 23:40:04 +0100

    feat: add stars to cv background

--- src/components/pages/cvPage.js
+++ src/components/pages/cvPage.js
@@ -2,21 +2,24 @@ import { CvLeftPane } from "../cv/cvLeftPane"
 import { CvHeader } from "../cv/cvHeader"
 import { CvRightPane } from "../cv/cvRighPane"
 import { PdfIcon } from "../pdfIcon"
+import { StarryNight } from "../starryNight"
 
 const CVPage = () => {
     return (
-        <div className="flex justify-center">
-            <div className="bg-white min-h-screen w-5/6 relative my-5 rounded-md pb-20 px-10">
-                <div className="absolute top-8 right-8">
-                    <PdfIcon />
-                </div>
-                <CvHeader />
-                <div className="flex lg:flex-row flex-col justify-evenly mt-10">
-                    <CvLeftPane />
-                    <CvRightPane />
+        <StarryNight>
+            <div className="flex justify-center">
+                <div className="bg-white min-h-screen w-5/6 relative my-5 rounded-md pb-20 px-10">
+                    <div className="absolute top-8 right-8">
+                        <PdfIcon />
+                    </div>
+                    <CvHeader />
+                    <div className="flex lg:flex-row flex-col justify-evenly mt-10">
+                        <CvLeftPane />
+                        <CvRightPane />
+                    </div>
                 </div>
             </div>
-        </div>
+        </StarryNight>
     )
 }
 
