c0a9f64608e67892212e35f1d7022e942f887f84
justjcurtis <justjcurtis@gmail.com>
2023-11-26 04:27:03 +0000

    perf: optimised star amount, placement, twinkling and rendering

--- src/components/starryNight.js
+++ src/components/starryNight.js
@@ -1,13 +1,14 @@
 import { useCallback, useEffect, useRef, useState } from "react"
 import { getRandomInt } from "../utils/helpers"
 
-const TWINKLE_RATE = 0.0008
+const TWINKLE_RATE = 0.001
 
 const StarryNight = ({ children }) => {
+    const maxDimension = Math.max(window.screen.width, window.screen.height)
     const randomXY = () => {
-        return { x: getRandomInt(window.innerWidth), y: getRandomInt(window.innerHeight * 2) }
+        return { x: getRandomInt(maxDimension), y: getRandomInt(maxDimension * 2) }
     }
-    const starCount = useRef(Math.floor((window.innerWidth * window.innerHeight) / 4000))
+    const starCount = useRef(Math.floor((maxDimension * maxDimension * 1.5) / 15000))
     const [stars, setStars] = useState(new Array(starCount.current).fill(0).map(() => ({ ...randomXY(), size: getRandomInt(2, 0.5), opacity: 1 })))
     const twinkling = useRef({})
 
@@ -15,8 +16,15 @@ const StarryNight = ({ children }) => {
         setStars((currentStars) => {
             if (!currentStars) return
             const nextStars = []
-            for (let i = 0; i < starCount.current; i++) {
+            for (let i = 0; i < currentStars.length; i++) {
                 const s = currentStars[i]
+                const isOffscreen = s.x > window.innerWidth ||
+                    s.y > window.innerHeight + window.scrollY * 0.1 ||
+                    s.y < window.scrollY * 0.1
+                if (isOffscreen) {
+                    nextStars.push(s)
+                    continue
+                }
                 if (twinkling.current[i] !== undefined) {
                     twinkling.current[i]--
                     if (twinkling.current[i] === 0) {
@@ -44,11 +52,14 @@ const StarryNight = ({ children }) => {
         <>
             <div className="bg-transparent text-white py-8 z-50 sticky top-0 mx-auto left-0 right-0 overflow-hidden">
                 {stars && stars.map((s, i) => {
-                    return (
+                    const isOffscreen = s.x > window.innerWidth ||
+                        s.y > window.innerHeight + window.scrollY * 0.1 ||
+                        s.y < window.scrollY * 0.1
+                    return isOffscreen ? null : (
                         <div key={i} id={`star-${i}`} style={
                             {
                                 opacity: s.opacity,
-                                top: `${s.y - 200 + (-window.scrollY * 0.1)}px`,
+                                top: `${s.y + (-window.scrollY * 0.1)}px`,
                                 left: `${s.x}px`,
                                 backgroundXolor: `white`,
                                 width: `${s.size}px`,
