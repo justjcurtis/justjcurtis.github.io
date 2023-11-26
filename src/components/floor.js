1bd8b01191c75d94def82013592c0ce5f26e2b15
justjcurtis <justjcurtis@gmail.com>
2023-11-26 05:06:28 +0000

    chore: remove console log

--- src/components/floor.js
+++ src/components/floor.js
@@ -9,7 +9,6 @@ const Floor = () => {
     useEffect(() => {
         const grassTemp = []
         const maxDimension = Math.max(window.screen.width, window.screen.height)
-        console.log(maxDimension)
         for (let x = 0; x <= maxDimension; x += 1) {
             if (Math.random() < grassChance) {
                 grassTemp.push({
