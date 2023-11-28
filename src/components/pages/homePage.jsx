f24b08f67da8d14c00a42e34ab0c0a856f374b99
justjcurtis <justjcurtis@gmail.com>
2023-11-28 13:33:45 +0000

    fix: disable selection of text on homescreen; fix: clickable hidden elements on homescreen

--- src/components/pages/homePage.jsx
+++ src/components/pages/homePage.jsx
@@ -18,7 +18,7 @@ const styles = {
     },
     subtext: {
         width: '100%',
-    }
+    },
 }
 
 const HomePage = () => {
@@ -49,12 +49,25 @@ const HomePage = () => {
     const todaysGame = selectFromArrWithFloat(allGames, getRandomForToday())
     const tomorrowGame = selectFromArrWithFloat(allGames, getRandomForToday(1))
 
+    const dynamicStyles = {
+        hide: {
+            visibility: !showInfo ? 'visible' : 'hidden',
+            opacity: !showInfo ? 1 : 0,
+            transition: 'all 0.4s ease-in-out'
+        },
+        show: {
+            visibility: showInfo ? 'visible' : 'hidden',
+            opacity: showInfo ? 1 : 0,
+            transition: 'all 0.4s ease-in-out'
+        }
+    }
+
     return (
-        <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }} exit={{ opacity: 0 }} style={{ top: 0, height: '100svh', overflow: 'hidden', backgroundImage: `url(${images.HomeBackground})` }} className="hero flex-1 fixed">
-            <div style={{ opacity: +(!showInfo), transition: 'opacity 0.25s' }} className="hero-overlay bg-opacity-60 backdrop-blur-sm"></div>
+        <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }} exit={{ opacity: 0 }} style={{ top: 0, height: '100svh', overflow: 'hidden', backgroundImage: `url(${images.HomeBackground})` }} className="select-none hero flex-1 fixed">
+            <div style={dynamicStyles.hide} className="hero-overlay bg-opacity-60 backdrop-blur-sm"></div>
             {todaysGame && <todaysGame.game />}
-            <div style={{ opacity: +(showInfo), transition: 'opacity 0.5s' }} className="hero-overlay bg-opacity-60 backdrop-blur-sm"></div>
-            <div style={{ opacity: +(showInfo), transition: 'opacity 0.5s ease-in-out' }} className="hero-content text-center text-neutral-content mt-[100px]">
+            <div style={dynamicStyles.show} className="hero-overlay bg-opacity-60 backdrop-blur-sm"></div>
+            <div style={dynamicStyles.show} className="hero-content text-center text-neutral-content mt-[100px]">
                 <div className="max-w-md">
                     <m.h1 initial={{ opacity: 0, x: -2, y: -10 }} animate={{ opacity: 1, x: 0, y: 0 }} transition={{ delay: 0.4, duration: (0.1 + (0.3 * Math.random())), ease: 'easeInOut' }} exit={{ opacity: 0, x: 2, y: -10 }} className="mb-5 text-5xl font-bold"><span className='text-primary bg-primary-content rounded-md px-1'>Hi 👋🏼</span></m.h1>
                     <div style={styles.topContainer}>
