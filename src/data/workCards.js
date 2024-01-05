252ea3ef672d59e2fc64d70fa2e6ad860c7bb97e
justjcurtis <justjcurtis@gmail.com>
2024-01-05 00:50:10 +0000

    feat: ensure client side ordering of work cards

--- src/data/workCards.js
+++ src/data/workCards.js
@@ -1,9 +1,9 @@
 import { gql, GraphQLClient } from "graphql-request";
 import { ENDPOINT } from "./constants";
+import { isNumber } from "../utils/helpers";
 
 const query = gql`{
   workCards{
-    createdAt
     bg
     title
     name
@@ -16,9 +16,16 @@ const query = gql`{
 }`
 
 const getWorkCards = async () => {
-    const client = new GraphQLClient(ENDPOINT)
-    const { workCards } = await client.request(query)
-    return workCards
+  const client = new GraphQLClient(ENDPOINT)
+  const { workCards } = await client.request(query)
+  workCards.sort((a, b) => {
+    if (a.name == "Work") return -1
+    if (b.name == "Work") return 1
+    let aYear = parseInt(a.title.split(" - ")[1])
+    let bYear = parseInt(b.title.split(" - ")[1])
+    return bYear - aYear
+  })
+  return workCards
 }
 
 export { getWorkCards }
