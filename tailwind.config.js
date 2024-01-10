7ee3d16d0db0f17264e196cc3d1899ece8aca99a
justjcurtis <justjcurtis@gmail.com>
2024-01-10 22:26:50 +0000

    chore: general little cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import daisyui from "daisyui"
+export default {
   content: [
     './index.html',
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,5 +8,5 @@ module.exports = {
   daisyui: {
     themes: ["dark"]
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 }
