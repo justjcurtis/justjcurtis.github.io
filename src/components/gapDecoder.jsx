edc17bae01579b834dce37495b18c4f8ebf008bf
justjcurtis <justjcurtis@gmail.com>
2025-07-30 20:26:49 +0100

    fix retireve

--- src/components/gapDecoder.jsx
+++ src/components/gapDecoder.jsx
@@ -85,7 +85,7 @@ export const GapDecoder = () => {
                             <button
                                 className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                                 onClick={() => {
-                                    navigator.clipboard.writeText(finalResult);
+                                    navigator.clipboard.writeText(finalResult.current);
                                 }}>
                                 Retrieve Gap
                             </button>
