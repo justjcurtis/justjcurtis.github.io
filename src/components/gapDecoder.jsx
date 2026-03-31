2e51e9673f2abae501db3af121ccabfcd6a36bed
justjcurtis <justjcurtis@gmail.com>
2026-04-01 00:32:33 +0100

    chore: beeplen++

--- src/components/gapDecoder.jsx
+++ src/components/gapDecoder.jsx
@@ -6,7 +6,7 @@ import { COMMANDS } from '../data/constants';
 const BEEP_DELAY = 450
 
 let timeout = null;
-const debouncedBeep = (freq, ms = 225, len = 100) => {
+const debouncedBeep = (freq, ms = 225, len = 150) => {
     if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => {
         playBeep(freq, len);
