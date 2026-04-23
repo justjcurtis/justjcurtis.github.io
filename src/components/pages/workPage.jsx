69ea011cc40fdb6a02c112e538afd3ea5030e0d1
justjcurtis <justjcurtis@gmail.com>
2026-04-23 21:34:17 +0100

    chore: ensure custom colors are included for tailwind

--- src/components/pages/workPage.jsx
+++ src/components/pages/workPage.jsx
@@ -6,7 +6,7 @@ const WorkPage = () => {
     const [workCards] = useQuery('workCards', getWorkCards)
     return (
         <div className='min-h-full w-full py-16'>
-            <div className="bg-red-800 bg-red-600 bg-amber-400 bg-[#0026A3] lg:row-span-2 lg:col-span-2 lg:col-span-3 lg:row-span-3" />
+            <div className="bg-red-800 bg-red-600 bg-amber-400 bg-[#0026A3] bg-[#3B2D6D] lg:row-span-2 lg:col-span-2 lg:col-span-3 lg:row-span-3" />
             <div className='max-w-7xl mx-auto' >
                 <div className="grid grid-cols-1 lg:grid-cols-4 gap-2 m-4 lg:gap-3">
                     {workCards && workCards.map((card, index) => (
